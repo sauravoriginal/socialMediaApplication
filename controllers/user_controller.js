428ea9d5a9c430e36164bb3f664ce53e48c7cc1c
sauravoriginal <sauravoriginal2110@gmail.com>
2023-12-26 19:12:33 +0530

    rendering pages for sign-in and sign-up added sucessfully

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -6,3 +6,11 @@ module .exports.profile =async (req,res)=>{
 // exports.signin =async (req,res)=>{
 //     return res.end("user sign in  page");
 // }
+module.exports.signUp = async (req,res)=>{
+    return res.render('user_sign_up.ejs',{
+        title: "Sign Up"
+   });}
+   module.exports.signIn = async (req,res)=>{
+    return res.render('user_sign_in.ejs',{
+        title: "Sign In"
+   });}
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,10 @@ const express= require('express') ;
 const userController  =require('../controllers/user_controller');
 const router = express.Router();
 router.get('/profile',userController.profile);
-// router.get('/signin',userController.signin);
+router.get('/sign-Up',userController.signUp);
+router.get('/sign-in',userController.signIn);
+
+
 
 // any further routes aftter localhost:8000/user  will be here
 
