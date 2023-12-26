d4565f070dc92b0e6c02d79744e746e245dcbbf8
sauravoriginal <sauravoriginal2110@gmail.com>
2023-12-26 18:51:15 +0530

    connection to mongnoose db added sucesfully

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const db = async () => {
+    try {
+        const db = await mongoose.connect('mongodb://127.0.0.1/social_media_app');
+        console.log('DB connected');
+        return db;
+    } catch (err) {
+        console.error('Error in MongoDB connection', err);
+        throw err;
+    }
+};
+
+// Call the function to connect to the database
+db();
+
+// Export the mongoose object for potential usage elsewhere
+module.exports = db;
