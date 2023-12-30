b5c9fec293a5159cdb371d27d5f81c4536e0a7a5
sauravoriginal <sauravoriginal2110@gmail.com>
2023-12-30 18:02:14 +0530

    display post and related user with mongo db populate method for user object instead of userid

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,19 @@
+const Post = require('../models/post');
 module .exports.home =async (req,res)=>{
-    console.log(req.cookie);
-    res.cookie('user_id',25);
+    // console.log(req.cookie);
+    // res.cookie('user_id',25);
     // return res.end("Social. media app");
-    return res.render('home.ejs',{
-         title: "Home"
-    });
+    try{
+        //populate the user of each post(by doing popultae 
+        // we have whole user object instead of only userobejct id)
+        const posts = await Post.find({}).populate('user').exec();
+        
+        return res.render('home.ejs',{
+            title: "Home",
+            posts:posts
+       });  
+    }catch(err){ 
+        console.log("error in displaying post",err);
+    }
+   
 }
