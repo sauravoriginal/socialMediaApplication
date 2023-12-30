b6caeadbf023d04930b08d8ea9f89fc17abcc429
sauravoriginal <sauravoriginal2110@gmail.com>
2023-12-30 18:21:43 +0530

    check authentication on creating a post

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 const Post = require('../controllers/post_controller');
 const express= require('express') ;
+const passport = require('passport');
 const router = express.Router();
 
-router.post('/create',Post.create);
+router.post('/create',passport.checkAuthentication,Post.create);
 
 
 module.exports = router;
