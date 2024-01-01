5855a3adc364f0b08cec7070c3847ebb52eb7e65
sauravoriginal <sauravoriginal2110@gmail.com>
2024-01-01 16:07:01 +0530

    deleting a autherized comment and also delete it's realted id from post schema comment array from db

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,6 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
+const { findById } = require('../models/user');
 
 module .exports.create =async (req,res)=>{
     try{
@@ -27,4 +28,35 @@ module .exports.create =async (req,res)=>{
         return;
     }
 
+}
+
+// deleting a comment
+module.exports.destroy =async(req,res)=>{
+    try{
+        const comment = await Comment.findById(req.params.id);//from url -/posts/destroy/:id
+          // req.user._id converted to string my mongoose req.user.id
+          // also comment.user is ref to user i.e userid
+           const currentpost = await Post.findById(comment.post);
+        
+            if(currentpost && currentpost.user == req.user.id){
+                // for deleting comment id from  comment array inside post
+                // store post id to fetch commentid from array
+                let postId =comment.post;
+                await comment.deleteOne(); // delete this comment
+                // delete comment id from array and update the post
+               const post= await Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}});
+              if(post){
+                return res.redirect('back');
+              }
+              return res.redirect('back'); //else
+    
+              }
+           
+        
+            return;
+    }catch(err){
+        console.log("Error in deleting a comment",err);
+
+        return;
+    }
 }
--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 const router = express.Router();
 
 router.post('/create',passport.checkAuthentication,commentsController.create);
-
+router.get('/destroy/:id',passport.checkAuthentication,commentsController.destroy);
 
 module.exports = router;
 
