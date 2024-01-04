d2dae6c5cd6c10842b5c6f862c8d30c2c444d15f
sauravoriginal <sauravoriginal2110@gmail.com>
2024-01-04 12:43:44 +0530

    created a method to create a form in DOM for receiving data in AJAX

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -10,8 +10,8 @@
             url:'/posts/create',
             data:newPostForm.serialize(),  //converts the post data intoo json
             success:(data)=>{
-                console.log(data);
-
+                let newPost = newPostDom(data.data.post);
+                $('#posts-list-container>ul').prepend(newPost);
             },
             error:(error)=>{
               console.log(error.responseText);
@@ -21,7 +21,33 @@
     });
   }
   // method to create a form in DOM
-  
+  let newPostDom = (post)=>{
+    return $(`<li id="post-${post._id}">
+    <p>
+            <small>
+                <a class="delete-post-button" href="/posts/destroy/${post._id}">X</a>
+            </small>
+        ${post.content}
+        <br>
+        <small>By  ${post.user.name}</small>
+    </p>
+    <div class="post-comments">
+       
+            <form action="/comments/create" method="post">
+                <input type="text" name="content" required="true" placeholder="Type your comment...">
+                <input type="hidden" name="post" value="${post._id}">
+                <input type="submit" value="Add Comment">
+            </form>
+        
+        <div class="post-comment-list">
+            <ul id="post-comments-${post._id}">
+                
+            </ul>
+        </div>
+    </div>
+</li>
+`)
+  }
 
   createPost();
 }
