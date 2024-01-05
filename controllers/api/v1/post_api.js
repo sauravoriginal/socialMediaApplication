c86a841f4807a03840a26cbce99e62b404166f1d
sauravoriginal <sauravoriginal2110@gmail.com>
2024-01-05 16:23:46 +0530

    get /api/v1/posts and delete /api/v1/posts/:id using postman checked sucessfully

--- controllers/api/v1/post_api.js
+++ controllers/api/v1/post_api.js
@@ -1,6 +1,48 @@
-module.exports.index =(req,res)=>{
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+module.exports.index =async (req,res)=>{
+    try{const posts = await Post.find({})
+    .sort('-createdAt') 
+    .populate('user')
+    .populate({
+         path:'comments',
+         populate: {
+            path:'user'
+         }
+    });
     return res.json(200,{
         message:"List of posts",
-        posts:[]
+        posts:posts
     });
+}catch(err){
+    console.log(err);
+}
+    
+}
+
+module.exports.destroy =async(req,res)=>{
+    try{
+          const post =await Post.findById(req.params.id);//from url -/posts/destroy/:id
+          // req.user._id converted to string my mongoose req.user.id
+          // also post.user is ref to user i.e userid
+   
+            await post.deleteOne();            // delete all comment associated with this post
+           const isCommentDltd= await Comment.deleteMany({post: req.params.id});
+          
+
+           return res.json(200,{
+            message:"post and its associated comments got deleted",
+            
+        });            
+           
+
+          
+    }catch(err){
+        console.log(err);
+        return res.json(500,{
+            message:"Internal server error",
+            
+            
+        });  
+    }
 }
